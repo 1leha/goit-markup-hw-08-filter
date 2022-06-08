99642f98e452988b3fdb261ec38241e66e53887e
Alexey Lisovoy <88471482+1leha@users.noreply.github.com>
2022-06-08 13:23:48 +0400

    adding JS-filter

---
+++ js/portfolio-data.js
@@ -0,0 +1,199 @@
+export default [
+  {
+    cardText:
+      'Технокряк это современная площадка распространения коронавируса. Компании используют эту платформу для цифрового шпионажа и атак на защищённые сервера конкурентов.',
+    urlDesktopSized: [
+      './img/portfolio/pfolio-img-1-370px.jpg',
+      './img/portfolio/pfolio-img-1-740px.jpg',
+    ],
+    urlTabletSized: [
+      './img/portfolio/pfolio-img-1-354px.jpg',
+      './img/portfolio/pfolio-img-1-708px.jpg',
+    ],
+    urlPhoneSized: [
+      './img/portfolio/pfolio-img-1-450px.jpg',
+      './img/portfolio/pfolio-img-1-900px.jpg',
+    ],
+    urlDefault: './img/portfolio/pfolio-img-1-450px.jpg',
+    altText: 'Технокряк. Веб-сайт',
+    cardTitle: 'Технокряк',
+    cardDescription: 'Веб-сайт',
+    cardCathegory: 'web',
+  },
+
+  {
+    cardText:
+      'Технокряк это современная площадка распространения коронавируса. Компании используют эту платформу для цифрового шпионажа и атак на защищённые сервера конкурентов.',
+    urlDesktopSized: [
+      './img/portfolio/pfolio-img-2-370px.jpg',
+      './img/portfolio/pfolio-img-2-740px.jpg',
+    ],
+    urlTabletSized: [
+      './img/portfolio/pfolio-img-2-354px.jpg',
+      './img/portfolio/pfolio-img-2-708px.jpg',
+    ],
+    urlPhoneSized: [
+      './img/portfolio/pfolio-img-2-450px.jpg',
+      './img/portfolio/pfolio-img-2-900px.jpg',
+    ],
+    urlDefault: './img/portfolio/pfolio-img-2-450px.jpg',
+    altText: 'Постер New Orlean vs Golden Star. Дизайн',
+    cardTitle: 'Постер New Orlean vs Golden Star',
+    cardDescription: 'Дизайн',
+    cardCathegory: 'design',
+  },
+
+  {
+    cardText:
+      'Технокряк это современная площадка распространения коронавируса. Компании используют эту платформу для цифрового шпионажа и атак на защищённые сервера конкурентов.',
+    urlDesktopSized: [
+      './img/portfolio/pfolio-img-3-370px.jpg',
+      './img/portfolio/pfolio-img-3-740px.jpg',
+    ],
+    urlTabletSized: [
+      './img/portfolio/pfolio-img-3-354px.jpg',
+      './img/portfolio/pfolio-img-3-708px.jpg',
+    ],
+    urlPhoneSized: [
+      './img/portfolio/pfolio-img-3-450px.jpg',
+      './img/portfolio/pfolio-img-3-900px.jpg',
+    ],
+    urlDefault: './img/portfolio/pfolio-img-3-450px.jpg',
+    altText: 'Ресторан Seafood. Приложение',
+    cardTitle: 'Ресторан Seafood',
+    cardDescription: 'Приложение',
+    cardCathegory: 'app',
+  },
+
+  {
+    cardText:
+      'Технокряк это современная площадка распространения коронавируса. Компании используют эту платформу для цифрового шпионажа и атак на защищённые сервера конкурентов.',
+    urlDesktopSized: [
+      './img/portfolio/pfolio-img-4-370px.jpg',
+      './img/portfolio/pfolio-img-4-740px.jpg',
+    ],
+    urlTabletSized: [
+      './img/portfolio/pfolio-img-4-354px.jpg',
+      './img/portfolio/pfolio-img-4-708px.jpg',
+    ],
+    urlPhoneSized: [
+      './img/portfolio/pfolio-img-4-450px.jpg',
+      './img/portfolio/pfolio-img-4-900px.jpg',
+    ],
+    urlDefault: './img/portfolio/pfolio-img-4-450px.jpg',
+    altText: 'Проект Prime. Маркетинг',
+    cardTitle: 'Проект Prime',
+    cardDescription: 'Маркетинг',
+    cardCathegory: 'marketing',
+  },
+
+  {
+    cardText:
+      'Технокряк это современная площадка распространения коронавируса. Компании используют эту платформу для цифрового шпионажа и атак на защищённые сервера конкурентов.',
+    urlDesktopSized: [
+      './img/portfolio/pfolio-img-5-370px.jpg',
+      './img/portfolio/pfolio-img-5-740px.jpg',
+    ],
+    urlTabletSized: [
+      './img/portfolio/pfolio-img-5-354px.jpg',
+      './img/portfolio/pfolio-img-5-708px.jpg',
+    ],
+    urlPhoneSized: [
+      './img/portfolio/pfolio-img-5-450px.jpg',
+      './img/portfolio/pfolio-img-5-900px.jpg',
+    ],
+    urlDefault: './img/portfolio/pfolio-img-5-450px.jpg',
+    altText: 'Проект Boxes. Приложение',
+    cardTitle: 'Проект Boxes',
+    cardDescription: 'Приложение',
+    cardCathegory: 'app',
+  },
+
+  {
+    cardText:
+      'Технокряк это современная площадка распространения коронавируса. Компании используют эту платформу для цифрового шпионажа и атак на защищённые сервера конкурентов.',
+    urlDesktopSized: [
+      './img/portfolio/pfolio-img-6-370px.jpg',
+      './img/portfolio/pfolio-img-6-740px.jpg',
+    ],
+    urlTabletSized: [
+      './img/portfolio/pfolio-img-6-354px.jpg',
+      './img/portfolio/pfolio-img-6-708px.jpg',
+    ],
+    urlPhoneSized: [
+      './img/portfolio/pfolio-img-6-450px.jpg',
+      './img/portfolio/pfolio-img-6-900px.jpg',
+    ],
+    urlDefault: './img/portfolio/pfolio-img-6-450px.jpg',
+    altText: 'Inspiration has no Borders. Веб-сайт',
+    cardTitle: 'Inspiration has no Borders',
+    cardDescription: 'Веб-сайт',
+    cardCathegory: 'web',
+  },
+
+  {
+    cardText:
+      'Технокряк это современная площадка распространения коронавируса. Компании используют эту платформу для цифрового шпионажа и атак на защищённые сервера конкурентов.',
+    urlDesktopSized: [
+      './img/portfolio/pfolio-img-7-370px.jpg',
+      './img/portfolio/pfolio-img-7-740px.jpg',
+    ],
+    urlTabletSized: [
+      './img/portfolio/pfolio-img-7-354px.jpg',
+      './img/portfolio/pfolio-img-7-708px.jpg',
+    ],
+    urlPhoneSized: [
+      './img/portfolio/pfolio-img-7-450px.jpg',
+      './img/portfolio/pfolio-img-7-900px.jpg',
+    ],
+    urlDefault: './img/portfolio/pfolio-img-7-450px.jpg',
+    altText: 'Издание Limited Edition. Дизайн',
+    cardTitle: 'Издание Limited Edition',
+    cardDescription: 'Дизайн',
+    cardCathegory: 'design',
+  },
+
+  {
+    cardText:
+      'Технокряк это современная площадка распространения коронавируса. Компании используют эту платформу для цифрового шпионажа и атак на защищённые сервера конкурентов.',
+    urlDesktopSized: [
+      './img/portfolio/pfolio-img-8-370px.jpg',
+      './img/portfolio/pfolio-img-8-740px.jpg',
+    ],
+    urlTabletSized: [
+      './img/portfolio/pfolio-img-8-354px.jpg',
+      './img/portfolio/pfolio-img-8-708px.jpg',
+    ],
+    urlPhoneSized: [
+      './img/portfolio/pfolio-img-8-450px.jpg',
+      './img/portfolio/pfolio-img-8-900px.jpg',
+    ],
+    urlDefault: './img/portfolio/pfolio-img-8-450px.jpg',
+    altText: 'Проект LAB. Маркетинг',
+    cardTitle: 'Проект LAB',
+    cardDescription: 'Маркетинг',
+    cardCathegory: 'marketing',
+  },
+
+  {
+    cardText:
+      'Технокряк это современная площадка распространения коронавируса. Компании используют эту платформу для цифрового шпионажа и атак на защищённые сервера конкурентов.',
+    urlDesktopSized: [
+      './img/portfolio/pfolio-img-9-370px.jpg',
+      './img/portfolio/pfolio-img-9-740px.jpg',
+    ],
+    urlTabletSized: [
+      './img/portfolio/pfolio-img-9-354px.jpg',
+      './img/portfolio/pfolio-img-9-708px.jpg',
+    ],
+    urlPhoneSized: [
+      './img/portfolio/pfolio-img-9-450px.jpg',
+      './img/portfolio/pfolio-img-9-900px.jpg',
+    ],
+    urlDefault: './img/portfolio/pfolio-img-9-450px.jpg',
+    altText: 'Growing Business. Приложение',
+    cardTitle: 'Growing Business',
+    cardDescription: 'Приложение',
+    cardCathegory: 'app',
+  },
+];
---
+++ js/portfolio.js
@@ -0,0 +1,148 @@
+import cards from './portfolio-data.js';
+
+const refs = {
+  //   filterButtons: document.querySelectorAll('.filter-label__radio-original'),
+  rbAll: document.querySelector('[data-cathegory="all"]'),
+  rbWeb: document.querySelector('[data-cathegory="web"]'),
+  rbApp: document.querySelector('[data-cathegory="app"]'),
+  rbDesign: document.querySelector('[data-cathegory="design"]'),
+  rbMarketing: document.querySelector('[data-cathegory="marketing"]'),
+  filterButtonsUl: document.querySelector('.filter'),
+  portfolioContainer: document.querySelector('.portfolio'),
+};
+let filteredCards;
+// console.log(cards);
+window.onload = e => {
+  cardMaker(cards);
+};
+
+refs.filterButtonsUl.addEventListener('click', e => {
+  //   console.log(e.target === refs.rbWeb);
+  switch (e.target) {
+    case refs.rbAll:
+      cardMaker(cards);
+      break;
+
+    case refs.rbWeb:
+      cardMaker(cards.filter(({ cardCathegory }) => cardCathegory === 'web'));
+      break;
+
+    case refs.rbApp:
+      cardMaker(cards.filter(({ cardCathegory }) => cardCathegory === 'app'));
+      break;
+
+    case refs.rbDesign:
+      cardMaker(cards.filter(({ cardCathegory }) => cardCathegory === 'design'));
+      break;
+
+    case refs.rbMarketing:
+      cardMaker(cards.filter(({ cardCathegory }) => cardCathegory === 'marketing'));
+      break;
+
+    default:
+      cardMaker(cards);
+      break;
+  }
+});
+
+function cardMaker(cards) {
+  const cardsToPaste = cards.map(
+    ({
+      cardText,
+      urlDesktopSized,
+      urlTabletSized,
+      urlPhoneSized,
+      urlDefault,
+      altText,
+      cardTitle,
+      cardDescription,
+    }) => {
+      return `<li class="portfolio__item">
+            <a href="" class="card">
+              <div class="card__thumb">
+                <div class="card__overlay">
+                  <p class="card__text">
+                    ${cardText}
+                  </p>
+                </div>
+                <picture class="card__image">
+                  <source
+                    srcset="
+                                  ${urlDesktopSized[0]} 1x,
+                                  ${urlDesktopSized[1]} 2x
+                                "
+                    media="(min-width: 1200px)"
+                  />
+                  <source
+                    srcset="
+                                  ${urlTabletSized[0]} 1x,
+                                  ${urlTabletSized[1]} 2x
+                                "
+                    media="(min-width: 768px)"
+                  />
+                  <source
+                    srcset="
+                                  ${urlPhoneSized[0]} 1x,
+                                  ${urlPhoneSized[1]} 2x
+                                "
+                    media="(min-width: 320px)"
+                  />
+                  <img src="${urlDefault}"
+                  alt="${altText}" />
+                </picture>
+              </div>
+              <div class="card__content">
+                <h2 class="card__title">${cardTitle}</h2>
+                <p class="card__description">${cardDescription}</p>
+              </div>
+            </a>
+          </li>`;
+    },
+  );
+  refs.portfolioContainer.innerHTML = '';
+  refs.portfolioContainer.insertAdjacentHTML('beforeend', cardsToPaste.join(''));
+}
+// {/* <li class="portfolio__item">
+//   <a href="" class="card">
+//     <div class="card__thumb">
+//       <div class="card__overlay">
+//         <p class="card__text">
+//           Технокряк это современная площадка распространения коронавируса. Компании используют эту
+//           платформу для цифрового шпионажа и атак на защищённые сервера конкурентов.
+//         </p>
+//       </div>
+
+//       <picture class="card__image">
+//         <source
+//           srcset="
+//                         ./img/portfolio/pfolio-img-1-370px.jpg 1x,
+//                         ./img/portfolio/pfolio-img-1-740px.jpg 2x
+//                       "
+//           media="(min-width: 1200px)"
+//         />
+
+//         <source
+//           srcset="
+//                         ./img/portfolio/pfolio-img-1-354px.jpg 1x,
+//                         ./img/portfolio/pfolio-img-1-708px.jpg 2x
+//                       "
+//           media="(min-width: 768px)"
+//         />
+
+//         <source
+//           srcset="
+//                         ./img/portfolio/pfolio-img-1-450px.jpg 1x,
+//                         ./img/portfolio/pfolio-img-1-900px.jpg 2x
+//                       "
+//           media="(min-width: 320px)"
+//         />
+//         <img src="./img/portfolio/pfolio-img-1-450px.jpg" alt="Технокряк. Веб-сайт" />
+//       </picture>
+//     </div>
+
+//     <div class="card__content">
+//       <h2 class="card__title">Технокряк</h2>
+//       <p class="card__description">Веб-сайт</p>
+//     </div>
+//   </a>
+// </li> */}
